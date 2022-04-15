2c383e4f854c37a1652f58e1bb5ad47c1dedbaa0
manager <manager@DIGTEC1>
2022-04-15 17:38:52 +0300

    Проверка даты

--- MigrateReviewsCore/Program.cs
+++ MigrateReviewsCore/Program.cs
@@ -16,7 +16,7 @@ namespace MigrateReviewsCore
             var name = "v.piskov@coffeemania.ru";
             var password = "dkYnjkdkjd77";
             var date = new DateTime(2022, 04, 13);
-            var feedback = new MigrateFeedbacks(name, password, "5", "2022-04-13");
+            var feedback = new MigrateFeedbacks(name, password, "5");
             feedback.GetFeedbackCloudTips();
             var fe = feedback.Feedbacks;
             var result = feedback.SetFeedBacksZendesk("vjW2dfbHWJWlDBtcNKp8GgaGCIL95WOvLdVDkmws", "a.yakovleva@coffeemania.ru", "https://coffeemania.zendesk.com/api/v2/tickets.json");
