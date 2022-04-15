8a5f1063eefdb7ed98bdc6ed89ff3ff624455cb5
manager <manager@DIGTEC1>
2022-04-15 14:59:58 +0300

    LogginInDB

---
+++ MigrateReviewsCore/DataBase/CloudTips.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MigrateReviewsCore.DataBase
+{
+
+    internal class CloudTips
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string Data { get; set; }
+        [Required]
+        public string Comment { get; set; }
+        
+    }
+}
---
+++ MigrateReviewsCore/DataBase/StreamingServiceContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrateReviewsCore.DataBase
+{
+    internal class StreamingServiceContext:DbContext
+    {
+        private readonly string ConnectionString;
+        public StreamingServiceContext(string connectionString)
+        { 
+            ConnectionString = connectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+        public DbSet<FeedbackCloudTips> feedbackCloudTips { get; set; }
+    }
+}
