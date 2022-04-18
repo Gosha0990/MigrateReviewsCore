a62a9e430fbc48f4b33df7e3491473b80b9f892b
manager <manager@DIGTEC1>
2022-04-18 16:58:56 +0300

    ResultTransactions

--- MigrateReviewsCore/Program.cs
+++ MigrateReviewsCore/Program.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using MigrateReviewsCore.Data;
-using MigrateReviewsCore.DataBase;
+﻿using MigrateReviewsCore.DataApi;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 
 namespace MigrateReviewsCore
 {
@@ -15,11 +10,11 @@ namespace MigrateReviewsCore
         {
             var name = "v.piskov@coffeemania.ru";
             var password = "dkYnjkdkjd77";
-            var date = new DateTime(2022, 04, 13);
-            var feedback = new MigrateFeedbacks(name, password, "5");
+            var feedback = new MigrateFeedbacks(name, password);
             feedback.GetFeedbackCloudTips();
             var fe = feedback.Feedbacks;
-            var result = feedback.SetFeedBacksZendesk("vjW2dfbHWJWlDBtcNKp8GgaGCIL95WOvLdVDkmws", "a.yakovleva@coffeemania.ru", "https://coffeemania.zendesk.com/api/v2/tickets.json");
+
+            var result = feedback.SetFeedBacksZendeskAndDb("vjW2dfbHWJWlDBtcNKp8GgaGCIL95WOvLdVDkmws", "a.yakovleva@coffeemania.ru", "https://coffeemania.zendesk.com/api/v2/tickets.json");
             Console.WriteLine(result);
 
         }
